d428c1410af01399324feef5372aebbd395e9fba
mobiletechtracker <info@mobiletechtracker.co.uk>
2019-12-07 16:49:14 +0000

    Add the content.

---
+++ 4-interface-segregation-principle/TextToHtmlConvertorTests/TextProcessorTests.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using TextToHtmlConvertor;
+using Xunit;
+
+namespace TextToHtmlConvertorTests
+{
+    public class TextProcessorTests
+    {
+        private readonly TextProcessor textProcessor;
+
+        public TextProcessorTests()
+        {
+            var tagsToReplace = new Dictionary<string, (string, string)>
+                {
+                    { "**", ("<strong>", "</strong>") },
+                    { "*", ("<em>", "</em>") },
+                    { "~~", ("<del>", "</del>") }
+                };
+
+            textProcessor = new MdTextProcessor(tagsToReplace);
+        }
+
+        [Fact]
+        public void CanConvertText()
+        {
+            var originalText = "This is the first paragraph. It has * and *.\r\n" +
+                "This is the second paragraph. It has ** and **.";
+
+            var expectedSting = "<p>This is the first paragraph. It has * and *.</p>\r\n" +
+                "<p>This is the second paragraph. It has ** and **.</p>\r\n" +
+                "<br/>\r\n";
+
+            Assert.Equal(expectedSting, textProcessor.ConvertText(originalText));
+        }
+    }
+}
